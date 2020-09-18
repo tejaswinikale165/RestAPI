9a56c5d8bae51eac0cf2c2e9840d947c17052dbc
tejaswinikale165 <tejaswinikale165@gmail.com>
2020-09-18 10:01:58 +0100

    Changed Code

--- src/com/tds/beans/Job_Title.java
+++ src/com/tds/beans/Job_Title.java
@@ -3,12 +3,12 @@ package com.tds.beans;
 public class Job_Title {
 public int Job_Tile_Code;
 public String Job_Title_Name;
-public String Departmanet_Name;
-public Job_Title(int job_Tile_Code, String job_Title_Name, String departmanet_Name) {
+public String Department_Name;
+public Job_Title(int job_Tile_Code, String job_Title_Name, String department_Name) {
 	super();
 	Job_Tile_Code = job_Tile_Code;
 	Job_Title_Name = job_Title_Name;
-	Departmanet_Name = departmanet_Name;
+	Department_Name = department_Name;
 }
 public int getJob_Tile_Code() {
 	return Job_Tile_Code;
@@ -22,11 +22,11 @@ public String getJob_Title_Name() {
 public void setJob_Title_Name(String job_Title_Name) {
 	Job_Title_Name = job_Title_Name;
 }
-public String getDepartmanet_Name() {
-	return Departmanet_Name;
+public String getDepartment_Name() {
+	return Department_Name;
 }
-public void setDepartmanet_Name(String departmanet_Name) {
-	Departmanet_Name = departmanet_Name;
+public void setDepartment_Name(String department_Name) {
+	Department_Name = department_Name;
 }
 
 
